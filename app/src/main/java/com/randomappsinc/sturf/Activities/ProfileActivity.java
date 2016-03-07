e65679e12f40a96e5e62ebb1b22de244bc8f57e1
Gear61 <chessnone@yahoo.com>
2016-03-06 21:22:26 -0800

    Stubbing profile

--- app/src/main/java/com/randomappsinc/sturf/Activities/ProfileActivity.java
+++ app/src/main/java/com/randomappsinc/sturf/Activities/ProfileActivity.java
@@ -1,20 +1,35 @@
 package com.randomappsinc.sturf.Activities;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.randomappsinc.sturf.R;
+import com.squareup.picasso.Picasso;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
 
 /**
  * Created by alexanderchiou on 3/2/16.
  */
 public class ProfileActivity extends StandardActivity {
+    @Bind(R.id.profile_image) ImageView profilePic;
+    @Bind(R.id.user_name) TextView userName;
+    @Bind(R.id.school) TextView school;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
         ButterKnife.bind(this);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Picasso.with(this)
+                .load("http://i.imgur.com/nYIEqa2.png")
+                .into(profilePic);
+
+        userName.setText("Alex Chiou");
+        school.setText("UCLA");
     }
 }
